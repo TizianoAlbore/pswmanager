39135741201da5e0327b1d479785550c15ca9629
Faggenza <ik3cco@gmail.com>
2024-11-22 12:09:47 +0100

    error check on modal

--- pw_frontend/lib/widgets/modal.dart
+++ pw_frontend/lib/widgets/modal.dart
@@ -14,7 +14,8 @@ class CustomModal extends StatefulWidget {
 }
 
 class _CustomModalState extends State<CustomModal> {
-
+  
+  final _formKey = GlobalKey<FormState>();
   bool _passwordVisible = false;
 
   // Toggles the password show status
@@ -35,51 +36,66 @@ class _CustomModalState extends State<CustomModal> {
     return AlertDialog(
       title: const Text('Add New Entry'),
       content: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: titleController,
-              decoration: const InputDecoration(labelText: 'Title'),
-            ),
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: _passwordVisible,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                suffixIcon: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.refresh),
-                      onPressed: () => passwordController.text = generatePassword(length: 6, digits: true, specialChars: true, capitalLetters: true),
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        _passwordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                        color: Theme.of(context).primaryColorDark,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: titleController,
+                decoration: const InputDecoration(labelText: 'Title'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: usernameController,
+                decoration: const InputDecoration(labelText: 'Username'),
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: _passwordVisible,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.refresh),
+                        onPressed: () => passwordController.text = generatePassword(length: 16, digits: true, specialChars: true, capitalLetters: true),
                       ),
-                      onPressed: () => _toggle(),
-                    ),
-                  ],
+                      IconButton(
+                        icon: Icon(
+                          _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                          color: Theme.of(context).primaryColorDark,
+                        ),
+                        onPressed: () => _toggle(),
+                      ),
+                    ],
+                ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  } 
+                  return null;
+                }
               ),
+              TextFormField(
+                controller: noteController,
+                decoration: const InputDecoration(labelText: 'Note'),
               ),
-            ),
-            TextField(
-              controller: noteController,
-              decoration: const InputDecoration(labelText: 'Note'),
-            ),
-            TextField(
-              controller: groupController,
-              decoration: const InputDecoration(labelText: 'Group'),
-            ),
-          ],
+              TextFormField(
+                controller: groupController,
+                decoration: const InputDecoration(labelText: 'Group'),
+              ),
+            ],
+          ),
         ),
       ),
       actions: [
@@ -92,6 +108,7 @@ class _CustomModalState extends State<CustomModal> {
         TextButton(
           onPressed: () async {
             // Handle save action
+            if (_formKey.currentState!.validate()) {
               try {
                 await addPassword(titleController.text, usernameController.text, passwordController.text, noteController.text, groupController.text, widget.firestore, widget.userId);
                 ScaffoldMessenger.of(context).showSnackBar(
@@ -100,12 +117,18 @@ class _CustomModalState extends State<CustomModal> {
               titleController.clear();
               passwordController.clear();
               noteController.clear();
+              groupController.clear();
+              Navigator.of(context).pop();
               } catch (e) {
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(content: Text('Failed to add password: ${e.toString()}')),
                 );
               }
-            Navigator.of(context).pop();
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Please enter valid data')),
+              );
+            }
           },
           child: const Text('Save'),
         ),
